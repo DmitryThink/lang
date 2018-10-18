f32fe7ab2f71fd62fc75b1e719363d50acb166a0
Dmytro Kuzhel <Dmytro.Kuzhel@besidethepark.com>
2018-10-18 12:55:29 +0200

    Add words to db

--- app/models/queue_list_item.rb
+++ app/models/queue_list_item.rb
@@ -8,7 +8,7 @@ class QueueListItem < ApplicationRecord
   after_save :set_time_of_appearance, if: :saved_change_to_index_of_time?
 
   validates :translation, uniqueness: { scope: :queue_list }
-  validates :index_of_time, :inclusion => -2..10
+  validates :index_of_time, :inclusion => -2..(self.list_of_time.count+1)
 
   def tip
     if queue_list.with_first_letter
@@ -19,14 +19,14 @@ class QueueListItem < ApplicationRecord
   end
 
   def list_of_time
-    [ 2, 4, 8, 16, 24, 32 ]
+    [ 1.minutes, 2.minutes, 10.minutes, 1.hours, 2.hours, 12.hours, 1.days, 1.months, 1.years ]
   end
 
   def set_time_of_appearance
     if self.index_of_time > self.list_of_time.count
       self.destroy!
     elsif self.index_of_time > 0
-      self.update!(time_of_appearance: Time.now + self.list_of_time[index_of_time-1].hours) # hours
+      self.update!(time_of_appearance: Time.now + self.list_of_time[index_of_time-1])
     end
   end
 end
