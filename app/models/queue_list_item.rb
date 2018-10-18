4f6ec998b9619f957912f2c2d6724ca846945ffa
Dmytro Kuzhel <Dmytro.Kuzhel@besidethepark.com>
2018-10-18 13:08:12 +0200

    Fix issue

--- app/models/queue_list_item.rb
+++ app/models/queue_list_item.rb
@@ -8,7 +8,7 @@ class QueueListItem < ApplicationRecord
   after_save :set_time_of_appearance, if: :saved_change_to_index_of_time?
 
   validates :translation, uniqueness: { scope: :queue_list }
-  validates :index_of_time, :inclusion => -2..(self.list_of_time.count+1)
+  validates :index_of_time, :inclusion => -2..10
 
   def tip
     if queue_list.with_first_letter
@@ -18,15 +18,15 @@ class QueueListItem < ApplicationRecord
     end
   end
 
-  def list_of_time
+  def self.list_of_time
     [ 1.minutes, 2.minutes, 10.minutes, 1.hours, 2.hours, 12.hours, 1.days, 1.months, 1.years ]
   end
 
   def set_time_of_appearance
-    if self.index_of_time > self.list_of_time.count
+    if self.index_of_time > QueueListItem.list_of_time.count
       self.destroy!
     elsif self.index_of_time > 0
-      self.update!(time_of_appearance: Time.now + self.list_of_time[index_of_time-1])
+      self.update!(time_of_appearance: Time.now + QueueListItem.list_of_time[index_of_time-1])
     end
   end
 end
